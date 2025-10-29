f4ceac154fba48013b307962c543f8d865109658
x3yavvy-cloud <x3yavvy@gmail.com>
2025-10-29 15:32:47 -0400

    Update streamlit_app.py
    added requests section

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 
@@ -38,4 +39,8 @@ if ingredients_list and name_on_order:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success("Your Smoothie is ordered, "+name_on_order+"!", icon="✅")
+
+# new section to display nutritional info
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
         
